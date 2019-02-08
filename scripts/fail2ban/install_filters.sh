7f1790a0ab6868c8c585cf78661189ac564fd94c
Zwilla <info@zwilla.de>
2019-02-08 15:02:17 +0100

    Update install_filters.sh
    current output:
    ~# fail2ban-client status bitcoind_banned
    Status for the jail: bitcoind_banned
    |- Filter
    |  |- Currently failed:	0
    |  |- Total failed:	10
    |  `- File list:	/home/bitcoin/.bitcoin/debug.log
    `- Actions
       |- Currently banned:	10
       |- Total banned:	10
       `- Banned IP list:	188.65.213.21 2a01:4f8:10a:37ee::2 94.237.44.67 88.99.167.175 77.50.50.70 2a03:b0c0:2:d0::4bc:2001 188.166.69.73 129.13.88.177 2a04:3544:1000:1510:b08f:6fff:fe1b:3007 2a00:1398:4:2a01::78

--- scripts/fail2ban/install_filters.sh
+++ scripts/fail2ban/install_filters.sh
@@ -1,4 +1,63 @@
+#!/bin/bash
+# this is for linux / ubuntu tested only at momnet
+# always install fail2ban from source!!
+# rewrite your path if needed
+
+su bitcoin
+cat >>/home/bitcoin/.bitcoin/bitcoin.conf <<\EOF
+# https://bitcointalk.org/index.php?topic=1380642.msg14097654#msg14097654
+logips=1
+EOF;
+
+su --
+
+if [ -f /etc/fail2ban/filter.d/bitcoin.conf ]; then
+cat >/etc/fail2ban/filter.d/bitcoin.conf <<\EOF
+# Fail2Ban configuration file for bitcoin
 #
-#
-#
+[Definition]
+failregex = .*receive version message: Why\? Because fuck u.*peeraddr=<HOST>:.*
+ignoreregex =
+EOF;
+fi;
+
+if [ -f /etc/fail2ban/filter.d/bitcoin_banned.conf ]; then
+cat >/etc/fail2ban/filter.d/bitcoin_banned.conf <<\EOF
+[Definition]
+failregex = .*connection from <HOST>:.* dropped
+ignoreregex =
+EOF;
+fi;
+
+if [ -f /etc/fail2ban/jail.local ]; then
+cat >/etc/fail2ban/jail.local <<\EOF
+[bitcoin]
+
+enabled = true
+port    = 8333
+filter  = bitcoin
+logpath = /home/bitcoin/.bitcoin/debug.log
+maxretry = 0
+bantime  = 1w
+findtime = 1d
+
+[bitcoind_banned]
+enabled = true
+port    = 8333
+filter = bitcoind_banned
+logpath = /home/bitcoin/.bitcoin/debug.log
+maxretry = 0
+bantime  = 1w
+findtime = 1d
+EOF;
+fi;
+
+touch /etc/fail2ban/filter.d/bitcoin.conf
+chown root:root /etc/fail2ban/filter.d/bitcoin.conf
+chmod 644 /etc/fail2ban/filter.d/bitcoin.conf
+
+
+fail2ban-client reload
+fail2ban-client status
+
 end;
